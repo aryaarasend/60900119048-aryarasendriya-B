ac5e73150805ea7745499406813a81f9632158a2
LAPTOP-MADMF8IO\ASUS <dewafira@gmail.com>
2021-11-09 11:13:59 +0700

    pengertian vcs

---
+++ resources/views/Pengertian-vcs.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>pengertian vcs</title>
+</head>
+
+<body>
+    <p>Version control adalah sebuah sistem yang merekam perubahan-perubahan dari sebuah berkas atau </p>
+</p>  sekumpulan berkas dari waktu ke waktu sehingga Anda dapat menilik kembali versi khusus suatu saat nanti.</p>
+<li> bazar</li>
+    <li>subversion</li>
+        <li>mercurial</li>
+            <li>cvs</li>
+                <li>rcs</li>
+</html>
